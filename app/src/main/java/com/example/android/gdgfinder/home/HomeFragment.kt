82b3429f5e97203ba67f09a2920216916d698127
Daniil <porvipop@mail.ru>
2022-01-28 12:50:09 +0300

    Add fonts, FAB, style

--- app/src/main/java/com/example/android/gdgfinder/home/HomeFragment.kt
+++ app/src/main/java/com/example/android/gdgfinder/home/HomeFragment.kt
@@ -5,8 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
 import com.example.android.gdgfinder.R
+import com.example.android.gdgfinder.databinding.HomeFragmentBinding
+import com.example.android.gdgfinder.databinding.HomeFragmentBindingImpl
 
 class HomeFragment : Fragment() {
 
@@ -20,9 +24,23 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        val binding = HomeFragmentBinding.inflate(inflater)
+
         val view = inflater.inflate(R.layout.home_fragment, container, false)
         viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+        binding.viewModel = viewModel
 
-        return view
+        viewModel.navigateToSearch.observe(viewLifecycleOwner, Observer { navigate ->
+            if (navigate == true) {
+                val navController = binding.root.findNavController()
+                navController.navigate(R.id.action_homeFragment_to_gdgListFragment)
+                viewModel.onNavigatedToSearch()
+            }
+        })
+
+
+
+        return binding.root
     }
 }
